a0de2d1b4e02bb5ea5446d0d0a3d947fbd5e6546
SaqibMehmood08 <113119552+SaqibMehmood08@users.noreply.github.com>
2023-08-02 21:44:35 +0500

    saqib

---
+++ storage/framework/views/6ed308b61fe0ee30df929fe74c463b45.php
@@ -0,0 +1,28 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <?php if(auth()->check() && auth()->user()->hasRole('editor')): ?>
+                <div class="card-header"><?php echo e(__('Editor Dashboard')); ?></div>
+                <?php endif; ?>
+
+                
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo e(__('You are logged in!')); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Laravel\custom_login\resources\views/home.blade.php ENDPATH**/ ?>
